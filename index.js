8fa86a054dd2ff40d1f1111ce85c50c52cd3f655
imxande <alerodriguezlarosa@gmail.com>
2020-05-05 16:41:06 -0400

    created set method

--- index.js
+++ index.js
@@ -10,9 +10,21 @@ class HashTable {
         }
         return hash;
     }
+
+    set(key, value){
+        const address = this._hash(key);
+        if (!this.data[address]){
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+    
 }
 
 // sanity check
 const newHash = new HashTable(50);
 console.log("*******************Hash Table*******************");
+console.log(newHash);
+newHash.set("apples", 10);
 console.log(newHash);
